82ada0748bcc9896e3228309c1247650bd18b84c
Jason Hubbard <jasonhubb@gmail.com>
2021-05-12 19:48:18 -0700

    updates mainpage sidebar

--- client/src/components/MainPageSidebar/MainPageSidebar.js
+++ client/src/components/MainPageSidebar/MainPageSidebar.js
@@ -10,61 +10,20 @@ import chatBubbleIcon from '../../assets/bubble.svg';
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100vh",
-        fontFamily: "OpenSans",
-        fontWeight: "600",
-        fontStyle: "normal",
-      },
-      image: {
-        backgroundImage: `linear-gradient(to bottom, RGBA(58, 141, 255, .85), RGBA(134, 185, 255, .85)),url('${sidebarPicture}')`,
-    
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      },
-      form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-      },
-      registerButton: {
-        boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-        color: "#3A8DFF",
-      },
-      loginButton: {
-        backgroundColor: "#3A8DFF",
-      },
-      topRow: {
-        margin: "25px",
-      },
       pictureText: {
         height: "100vh",
         color: "white",
         backgroundImage: `linear-gradient(to bottom, RGBA(58, 141, 255, .85), RGBA(134, 185, 255, .85)),url('${sidebarPicture}')`,
-    
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        
       },
-    
-      ButtonGrid: {
-        margin: "30px",
+      chatBubble: {
+          margin: "50px 0",
       },
-      formHeader: {
-        fontWeight: "600",
-        fontStyle: "normal",
-      },
-      formGrid: {
-        margin: "50px 0",
-      },
-      registerButtonGrid: {
-        margin: "0 50px 0 0",
+      chatBubbleTextGrid: {
+          padding: "0 0 220px 0",
       }
-    
 }));
 
 const MainPageSidebar = () => {
@@ -78,11 +37,12 @@ const MainPageSidebar = () => {
     justify="center"
     align="center"
   >
-    <Grid item xs={9} sm={9} md={9}>
+    <Grid item xs={9} className={classes.chatBubbleTextGrid}>
       <Icon>
         <img
           src={chatBubbleIcon}
           alt="Chat Bubble"
+          className = {classes.chatBubble}
         />
       </Icon>
       <Typography variant="h4">Converse with anyone with any languages</Typography>
