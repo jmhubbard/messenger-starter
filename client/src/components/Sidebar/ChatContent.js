4ad281f6f2b684ed4cff62caae3945d441443af1
Jason Hubbard <jasonhubb@gmail.com>
2021-05-19 21:19:33 -0700

    creates a notification icon that uses the conversations message count as the number that is displayed.

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,6 +51,9 @@ const ChatContent = (props) => {
           {latestMessageText}
         </Typography>
       </Box>
+      <Typography className={classes.notification}>
+          {conversation.messageCount}
+      </Typography>
     </Box>
   );
 };
