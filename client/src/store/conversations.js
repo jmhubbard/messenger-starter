3a1af5317c607fd77d170e74d6a4cf9f9d6f44b9
Jason Hubbard <jasonhubb@gmail.com>
2021-05-19 21:25:16 -0700

    creates RESET_NOTIFICATIONS action/reducer that uses the resetConversationNotification function

--- client/src/store/conversations.js
+++ client/src/store/conversations.js
@@ -4,6 +4,7 @@ import {
   addSearchedUsersToStore,
   removeOfflineUserFromStore,
   addMessageToStore,
+  resetConversationNotifications,
 } from "./utils/reducerFunctions";
 
 // ACTIONS
@@ -15,6 +16,7 @@ const REMOVE_OFFLINE_USER = "REMOVE_OFFLINE_USER";
 const SET_SEARCHED_USERS = "SET_SEARCHED_USERS";
 const CLEAR_SEARCHED_USERS = "CLEAR_SEARCHED_USERS";
 const ADD_CONVERSATION = "ADD_CONVERSATION";
+const RESET_NOTIFICATIONS = "RESET_NOTIFICATIONS";
 
 // ACTION CREATORS
 
@@ -67,6 +69,13 @@ export const addConversation = (recipientId, newMessage) => {
   };
 };
 
+export const resetNotifications = (conversationId) => {
+  return {
+    type: RESET_NOTIFICATIONS,
+    payload: conversationId ,
+  };
+};
+
 // REDUCER
 
 const reducer = (state = [], action) => {
@@ -91,6 +100,8 @@ const reducer = (state = [], action) => {
         action.payload.recipientId,
         action.payload.newMessage
       );
+    case RESET_NOTIFICATIONS:
+      return resetConversationNotifications(state, action.payload);
     default:
       return state;
   }
