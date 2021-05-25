e79f4d69167784807472b22dccf375b87bbf85ea
Jason Hubbard <jasonhubb@gmail.com>
2021-05-24 21:24:00 -0700

    Adds updateMessageStatusDb, updateNotifcationCount and setMessageToReadInStore to the handleClick function. Also runs updateMessageStatusDb and setMessagesToReadInStore in the user is currently viewing a conversation that is receiving messages.

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -4,7 +4,8 @@ import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { withStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
 import { connect } from "react-redux";
-import { updateMessageStatus } from '../../store/utils/thunkCreators';
+import { updateMessageStatusDb, updateNotificationCount, setMessagesToReadInStore } from '../../store/utils/thunkCreators';
+import store from "../../store";
 
 const styles = {
   root: {
@@ -21,12 +22,39 @@ const styles = {
 };
 
 class Chat extends Component {
+  componentDidUpdate(){
+    const fullStore = store.getState();
+    const activeConversation = fullStore.activeConversation;
+    const lastMessage = this.props.conversation.messages[this.props.conversation.messages.length - 1];
+    const otherUser = this.props.conversation.otherUser.username;
+    const otherUserId = this.props.conversation.otherUser.id;
+
+    if (activeConversation === otherUser && lastMessage.senderId === otherUserId && lastMessage.readStatus === false) {
+      const reqBody = {
+        conversationId: this.props.conversation.id,
+      };
+      //If the user is currently viewing a conversation that is recieving messages
+      //the messages status will be updated to readStatus = true inside the database
+      //and the message status will be updated to readStatus = true inside the store
+      //There is no need to change notification count because the notification count only increases if the user isn't viewing the chat.
+      // This is done when a message is added to the store from socket.io
+      this.props.updateMessageStatusDb(reqBody);
+      this.props.setMessagesToReadInStore(reqBody);
+    }
+}
+
   handleClick = async (conversation) => {
     await this.props.setActiveChat(conversation.otherUser.username);
+    const conversationId = conversation.id;
     const reqBody = {
-      conversationId: conversation.id,
+      conversationId: conversationId,
     };
-    await this.props.updateMessageStatus(reqBody);
+    //Updates the all of the users unread messages to readStatus = true inside the database 
+    await this.props.updateMessageStatusDb(reqBody);
+    //Sets the conversation.messageCount to zero inside store
+    await this.props.updateNotificationCount(reqBody);
+    //Sets all of the users unread messages to readStatus = true inside the store
+    await this.props.setMessagesToReadInStore(reqBody);
   };
 
   render() {
@@ -54,8 +82,14 @@ const mapDispatchToProps = (dispatch) => {
     setActiveChat: (id) => {
       dispatch(setActiveChat(id));
     },
-    updateMessageStatus: (id) => {
-      dispatch(updateMessageStatus(id));
+    updateMessageStatusDb: (id) => {
+      dispatch(updateMessageStatusDb(id));
+    },
+    updateNotificationCount: (conversationId) => {
+      dispatch(updateNotificationCount(conversationId));
+    },
+    setMessagesToReadInStore: (conversationId) => {
+      dispatch(setMessagesToReadInStore(conversationId));
     }
   };
 };
