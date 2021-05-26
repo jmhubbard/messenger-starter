a3b735e5a3f05fd80c324094441ee43503fe019c
Jason Hubbard <65915304+jmhubbard@users.noreply.github.com>
2021-05-25 19:02:12 -0700

    Update client/src/store/utils/reducerFunctions.js
    Co-authored-by: John Sciutto <31010488+johnsciutto@users.noreply.github.com>

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -21,7 +21,7 @@ export const addMessageToStoreReceiver = (state, message, sender, currentActiveC
       convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       if (sender !== currentActiveConversation) {
-        convoCopy.notificationCount ++;
+        convoCopy.notificationCount++;
       };
 
 
