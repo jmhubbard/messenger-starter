84602ffac17af9fcb9ecaa1633f25401d4ef13dc
Jason Hubbard <jasonhubb@gmail.com>
2021-05-29 14:12:29 -0700

    Moves all the socket.on events outside of the connect event

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,27 +3,32 @@ import store from "./store";
 import {
   removeOfflineUser,
   addOnlineUser,
-  setNewMessageReceiver
+  setNewMessageReceiver,
 } from "./store/conversations";
 
 const socket = io(window.location.origin, { autoConnect: false });
 
-
 socket.on("connect", () => {
   console.log("connected to server");
+});
 
-  socket.on("add-online-user", (id) => {
-    store.dispatch(addOnlineUser(id));
-  });
+socket.on("add-online-user", (id) => {
+  store.dispatch(addOnlineUser(id));
+});
 
-  socket.on("remove-offline-user", (id) => {
-    store.dispatch(removeOfflineUser(id));
-  });
-  socket.on("new-message", (data) => {
-    const fullStore = store.getState();
-    const currentActiveConversation = fullStore.activeConversation;
-    store.dispatch(setNewMessageReceiver(data.message, data.from.username, currentActiveConversation));
-  });
+socket.on("remove-offline-user", (id) => {
+  store.dispatch(removeOfflineUser(id));
+});
+socket.on("new-message", (data) => {
+  const fullStore = store.getState();
+  const currentActiveConversation = fullStore.activeConversation;
+  store.dispatch(
+    setNewMessageReceiver(
+      data.message,
+      data.from.username,
+      currentActiveConversation
+    )
+  );
 });
 
 export default socket;
